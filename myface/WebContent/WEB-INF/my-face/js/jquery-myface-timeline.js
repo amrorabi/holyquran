5c52088060017badaf35c7ac7d388e853e0122bc
amrorabi@gmail.com <amrorabi@gmail.com@bbc3b9f3-93fd-8df4-27cf-8557701cf663>
2014-08-18 12:03:26 +0000

    fixed timeago issue

--- myface/WebContent/WEB-INF/my-face/js/jquery-myface-timeline.js
+++ myface/WebContent/WEB-INF/my-face/js/jquery-myface-timeline.js
@@ -1,7 +1,7 @@
 var accessToken = "CAACEdEose0cBAMjL3ZCPSj0DjkDlUX1YIATlmTjJ0TSWiUdcfH7PCL1SJJXIeTk5y0BZAq02GrMZBTT2M0ZCXlgY2w3dGI6aK9o27KoCe9EkvfZA0HtmZBI0ludX8jbLuGMXY234Y48klUiN6Bl2zOYTuya0e0QQwUyZBAh7aSb0uKSCv7l8dx1ekHfge3hvDEQSQCZCcm9stVkFjZCUmLy0c";
 
 jQuery.ajaxSetup({
- 	'complete': function() { jQuery('abbr.timeago').timeago(); alert('2');}
+ 	'complete': function() { jQuery('abbr.timeago').timeago(); }
 });
 
 $(document).ready(		
@@ -16,29 +16,27 @@ $(document).ready(
 				dataType : "json",
 				success : function(data, status, jqXHR) {
 
-					alert('1');
-					$('#timeline').append("<abbr class=\"timeago\" title=\"2008-07-17T09:24:17Z\">");
-//					var dateLabel = "";
-//					$.each(data.data, function(key, value) {
-//						//Creation date
-//						var creationDate = new Date(value.created_time);
-//						if(dateLabel != creationDate.toDateString()){
-//							dateLabel = creationDate.toDateString();
-//							var formattedDate = creationDate.toLocaleFormat('%d %b %Y'); 
-//							$('#timeline').append("<li class=\"time-label\"><span class=\"bg-red\"> " + formattedDate + " </span></li>");
-//						}
-//						
-//						//Post data
-//						$('#timeline').append("<li><i class=\"fa fa-user bg-aqua\"></i>" +
-//								"<div class=\"timeline-item\">" +
-//									"<span class=\"time\"><i class=\"fa fa-clock-o\"></i> " +
-//									"<abbr class=\"timeago\" title=\"2008-07-17T09:24:17Z\">" +
-//										"</span>" +
-//									"<h3 class=\"timeline-header no-border\">" +
-//										"<a href=\"#\">" + value.from.name + "</a> " + value.message +
-//									"</h3>" +
-//								"</div></li>");
-//					});
+					var dateLabel = "";
+					$.each(data.data, function(key, value) {
+						//Creation date
+						var creationDate = new Date(value.created_time);
+						if(dateLabel != creationDate.toDateString()){
+							dateLabel = creationDate.toDateString();
+							var formattedDate = creationDate.toLocaleFormat('%d %b %Y'); 
+							$('#timeline').append("<li class=\"time-label\"><span class=\"bg-red\"> " + formattedDate + " </span></li>");
+						}
+						
+						//Post data
+						$('#timeline').append("<li><i class=\"fa fa-user bg-aqua\"></i>" +
+								"<div class=\"timeline-item\">" +
+									"<span class=\"time\"><i class=\"fa fa-clock-o\"></i> " +
+									"<abbr class=\"timeago\" title='"+ value.created_time +"'>" +
+										"</span>" +
+									"<h3 class=\"timeline-header no-border\">" +
+										"<a href=\"#\">" + value.from.name + "</a> </br>" + value.message +
+									"</h3>" +
+								"</div></li>");
+					});
 
 				}
 			})
