f49205a683952cda59d043db36d30115c7314762
amrorabi@gmail.com <amrorabi@gmail.com@bbc3b9f3-93fd-8df4-27cf-8557701cf663>
2014-08-19 16:59:02 +0000

    added likes names

--- myface/WebContent/WEB-INF/my-face/js/jquery-myface-timeline.js
+++ myface/WebContent/WEB-INF/my-face/js/jquery-myface-timeline.js
@@ -1,8 +1,21 @@
 var accessToken = "CAACEdEose0cBAMjL3ZCPSj0DjkDlUX1YIATlmTjJ0TSWiUdcfH7PCL1SJJXIeTk5y0BZAq02GrMZBTT2M0ZCXlgY2w3dGI6aK9o27KoCe9EkvfZA0HtmZBI0ludX8jbLuGMXY234Y48klUiN6Bl2zOYTuya0e0QQwUyZBAh7aSb0uKSCv7l8dx1ekHfge3hvDEQSQCZCcm9stVkFjZCUmLy0c";
 
 jQuery.ajaxSetup({
- 	'complete': function() { jQuery('abbr.timeago').timeago(); }
+ 	'complete': function() { 
+ 		jQuery('abbr.timeago').timeago();		  //initialize timeago plugin
+ 		$("[data-toggle='tooltip']").tooltip();	  //activate tooltip feature in bootstrap
+ 	}
 });
+
+function getLikesNames(data){
+	var names = "";
+	$.each(data, function getName(key, value){
+		names += value.name;
+		names += "</br>";
+	});
+	
+	return names;
+}
 
 $(document).ready(		
 		function() {
@@ -29,7 +42,7 @@ $(document).ready(
 						var timeLineItem = "<li><i class=\"fa fa-user bg-aqua\"></i>" +
 							"<div class=\"timeline-item\">" +
 							"<span class=\"time\"><i class=\"fa fa-clock-o\"></i> " +
-							"<abbr class=\"timeago\" title='"+ value.created_time +"'>" +
+							"<abbr class=\"timeago\" title=\""+ value.created_time +"\">" +
 								"</span>" +
 							"<h3 class=\"timeline-header no-border\">" +
 								"<a href=\"#\">" + value.from.name + "</a> </br>" + value.message +
@@ -41,7 +54,8 @@ $(document).ready(
 						
 						if(value.likes != null){
 							timeLineItem += 
-								"<a href=\"#\" >" +
+								"<a href=\"#\" data-toggle=\"tooltip\" data-placement=\"top\" data-html=\"true\" " +
+								"title=\"" + getLikesNames(value.likes.data) + "\">" +
                                 value.likes.data.length + " likes </a>";							
 						}
 						
