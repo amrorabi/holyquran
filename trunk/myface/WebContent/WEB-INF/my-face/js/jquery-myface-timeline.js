d1cc66a8d36160abf378a8db6d4cc3a12417d69c
amrorabi@gmail.com <amrorabi@gmail.com@bbc3b9f3-93fd-8df4-27cf-8557701cf663>
2014-08-23 19:09:27 +0000

    -handled comments window -restructure the code

--- trunk/myface/WebContent/WEB-INF/my-face/js/jquery-myface-timeline.js
+++ trunk/myface/WebContent/WEB-INF/my-face/js/jquery-myface-timeline.js
@@ -7,74 +7,73 @@ jQuery.ajaxSetup({
  	}
 });
 
-function getLikesNames(data){
-	var names = "";
-	$.each(data, function getName(key, value){
-		names += value.name;
-		names += "</br>";
-	});
-	
-	return names;
-}
+function loadUserHome(){
+	$.ajax({
+		url : "https://graph.facebook.com/v2.1/me/home?access_token=" + accessToken,
+		type : "GET",
+		Accept : 'application/json',
+		contentType : 'application/json',
+		dataType : "json",
+		success : function(data, status, jqXHR) {
 
-$(document).ready(		
-		function() {
-			// load user home
-			$.ajax({
-				url : "https://graph.facebook.com/v2.1/me/home?access_token="
-						+ accessToken,
-				type : "GET",
-				Accept : 'application/json',
-				contentType : 'application/json',
-				dataType : "json",
-				success : function(data, status, jqXHR) {
-
-					var dateLabel = "";
-					$.each(data.data, function(key, value) {
-						//Creation date
-						var creationDate = new Date(value.created_time);
-						if(dateLabel != creationDate.toDateString()){
-							dateLabel = creationDate.toDateString();
-							var formattedDate = creationDate.toLocaleFormat('%d %b %Y'); 
-							$('#timeline').append("<li class=\"time-label\"><span class=\"bg-red\"> " + formattedDate + " </span></li>");
-						}
-						
-						var timeLineItem = "<li><i class=\"fa fa-user bg-aqua\"></i>" +
-							"<div class=\"timeline-item\">" +
-							"<span class=\"time\"><i class=\"fa fa-clock-o\"></i> " +
-							"<abbr class=\"timeago\" title=\""+ value.created_time +"\">" +
-								"</span>" +
-							"<h3 class=\"timeline-header no-border\">" +
-								"<a href=\"#\">" + value.from.name + "</a> </br>" + value.message +
-							"</h3>";
-						
-						if(value.likes != null || value.comments != null)
-							timeLineItem += 
-								" <div class=\"timeline-footer\">";
-						
-						if(value.likes != null){
-							timeLineItem += 
-								"<a href=\"#\" data-toggle=\"tooltip\" data-placement=\"top\" data-html=\"true\" " +
-								"title=\"" + getLikesNames(value.likes.data) + "\">" +
-                                value.likes.data.length + " likes </a>";							
-						}
-						
-						if(value.comments != null){
-							timeLineItem += "&nbsp;<a href=\"#\" >" +
-                                            value.comments.data.length + " comments </a>";
-						}
-						if(value.likes != null || value.comments != null)
-							timeLineItem += "</div>";
-						
-						timeLineItem +=	"</div></li>";
-						
-						//Post data
-						$('#timeline').append(timeLineItem);
-					});
-
+			var dateLabel = "";
+			$.each(data.data, function(key, value) {
+				//Creation date
+				var creationDate = new Date(value.created_time);
+				if(dateLabel != creationDate.toDateString()){
+					dateLabel = creationDate.toDateString();
+					var formattedDate = creationDate.toLocaleFormat('%d %b %Y'); 
+					$('#timeline').append("<li class=\"time-label\"><span class=\"bg-red\"> " + formattedDate + " </span></li>");
 				}
-			})
+				
+				var likesNames = getLikesNames(value.likes.data);
+				
+				var timeLineItem = "<li><i class=\"fa fa-user bg-aqua\"></i>" +
+					"<div class=\"timeline-item\">" +
+					
+					"<span class=\"time\"><i class=\"fa fa-clock-o\"></i> " +
+						"<abbr class=\"timeago\" title=\""+ value.created_time +"\"/>" +
+						"&nbsp&nbsp&nbsp&nbsp" +
+						" <button class=\"btn btn-sm like-btn\" value=" + value.id + ">" +
+						"<i class=\"fa fa-fw " + likeBtnClass + " like-icon\"></i></button>" +
+					"</span>" +
+					
+					"<h3 class=\"timeline-header no-border\">" +
+						"<a href=\"#\">" + value.from.name + "</a> </br>" + value.message +
+					"</h3>";
+				
+				timeLineItem += 
+						" <div class=\"timeline-footer\">";
+				
+				if(value.likes != null){
+					timeLineItem += 
+						"<a href=\"#\" class=\"likes-link\" data-toggle=\"tooltip\" data-placement=\"top\" data-html=\"true\" " +
+						"title=\"" + likesNames + "\">" +
+                        value.likes.data.length + " likes </a>";							
+				}
+				
+				if(value.comments != null){
+					timeLineItem += "&nbsp;<a href=\""+ value.id +"\" class=\"comments-link\">" +
+                                    value.comments.data.length + " comments </a>";
+				}
+				else{
+					timeLineItem += "&nbsp;<a href=\""+ value.id +"\" class=\"comments-link\">write comment</a>";
+				}
+				
+				timeLineItem += "</div>";
+				
+				timeLineItem +=	"</div></li>";
+				
+				//Post data
+				$('#timeline').append(timeLineItem);
+			});
 
 		}
-		
-);
+	});
+
+}
+
+$(document).ready(function() {
+		// load user home
+		loadUserHome();
+});
