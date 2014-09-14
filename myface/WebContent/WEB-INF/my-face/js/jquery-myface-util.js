ecd8972e254488303b9a73d43bbf33c2d7f86992
amrorabi@gmail.com <amrorabi@gmail.com@bbc3b9f3-93fd-8df4-27cf-8557701cf663>
2014-09-14 11:59:48 +0000

    handled text more/less feature

---
+++ myface/WebContent/WEB-INF/my-face/js/jquery-myface-util.js
@@ -0,0 +1,47 @@
+var config = {
+		showChars : 300,
+		ellipsesText : "...",
+		moreText : "more",
+		lessText : "less",
+		errMsg : null,
+		force : false
+	};
+
+
+$(document).ready(function($) {
+	$(document).on({
+		click : function() {
+			var $this = $(this);
+			if ($this.hasClass('less')) {
+				$this.removeClass('less');
+				$this.html(config.moreText);
+				$this.parent().prev().prev().show(); // shortcontent
+				$this.parent().prev().hide(); // allcontent
+			} else {
+				$this.addClass('less');
+				$this.html(config.lessText);
+				$this.parent().prev().prev().hide(); // shortcontent
+				$this.parent().prev().show(); // allcontent
+			}
+			return false;
+		}
+	}, '.morelink');
+});
+
+function getTextElement(content) {
+	
+	var contentlen = content.length;
+	
+	if (contentlen > config.showChars) {
+		
+		var c = content.substr(0, config.showChars) + config.ellipsesText;
+		return '<div class="shortcontent">' + c
+				+ '</div><div class="allcontent" style="display: none; margin-bottom:0">'
+				+ content
+				+ '</div><span><a href="javascript://nop/" class="morelink">'
+				+ config.moreText + '</a></span>';
+	}
+	else{
+		return content;
+	}
+}
